85d3198f7e0f8183c7461416270ddf6e40b69465
Pratik Kansara <pratik.kansara@mindinventory.com>
2024-03-04 14:28:32 +0530

    Resolved issue#36 Video url is not working

--- lib/src/vimeo_player.dart
+++ lib/src/vimeo_player.dart
@@ -68,7 +68,7 @@ class _VimeoVideoPlayerState extends State<VimeoVideoPlayer> {
   VideoPlayerController? _videoPlayerController;
 
   final VideoPlayerController _emptyVideoPlayerController =
-      VideoPlayerController.networkUrl(Uri.parse(''));
+  VideoPlayerController.networkUrl(Uri.parse(''));
 
   /// flick manager to manage the flick player
   FlickManager? _flickManager;
@@ -190,7 +190,7 @@ class _VimeoVideoPlayerState extends State<VimeoVideoPlayer> {
 
       _flickManager = FlickManager(
         videoPlayerController:
-            _videoPlayerController ?? _emptyVideoPlayerController,
+        _videoPlayerController ?? _emptyVideoPlayerController,
         autoPlay: widget.autoPlay,
         // ignore: use_build_context_synchronously
       )..registerContext(context);
@@ -207,28 +207,28 @@ class _VimeoVideoPlayerState extends State<VimeoVideoPlayer> {
         builder: (context, bool isVideo, child) => Container(
           child: isVideo
               ? FlickVideoPlayer(
-                  key: ObjectKey(_flickManager),
-                  flickManager: _flickManager ??
-                      FlickManager(
-                        videoPlayerController: _emptyVideoPlayerController,
-                      ),
-                  systemUIOverlay: widget.systemUiOverlay,
-                  preferredDeviceOrientation: widget.deviceOrientation,
-                  flickVideoWithControls: const FlickVideoWithControls(
-                    videoFit: BoxFit.fitWidth,
-                    controls: FlickPortraitControls(),
-                  ),
-                  flickVideoWithControlsFullscreen:
-                      const FlickVideoWithControls(
-                    controls: FlickLandscapeControls(),
-                  ),
-                )
-              : const Center(
-                  child: CircularProgressIndicator(
-                    color: Colors.grey,
-                    backgroundColor: Colors.white,
-                  ),
+            key: ObjectKey(_flickManager),
+            flickManager: _flickManager ??
+                FlickManager(
+                  videoPlayerController: _emptyVideoPlayerController,
                 ),
+            systemUIOverlay: widget.systemUiOverlay,
+            preferredDeviceOrientation: widget.deviceOrientation,
+            flickVideoWithControls: const FlickVideoWithControls(
+              videoFit: BoxFit.fitWidth,
+              controls: FlickPortraitControls(),
+            ),
+            flickVideoWithControlsFullscreen:
+            const FlickVideoWithControls(
+              controls: FlickLandscapeControls(),
+            ),
+          )
+              : const Center(
+            child: CircularProgressIndicator(
+              color: Colors.grey,
+              backgroundColor: Colors.white,
+            ),
+          ),
         ),
       ),
       onPopInvoked: (didPop) {
@@ -240,25 +240,33 @@ class _VimeoVideoPlayerState extends State<VimeoVideoPlayer> {
 
   /// used to get valid vimeo video configuration
   Future<VimeoVideoConfig?> _getVimeoVideoConfigFromUrl(
-    String url, {
-    bool trimWhitespaces = true,
-  }) async {
+      String url, {
+        bool trimWhitespaces = true,
+      }) async {
     if (trimWhitespaces) url = url.trim();
 
     /// here i'm converting the vimeo video id only and calling config api for vimeo video .mp4
     /// supports this types of urls
-    /// https://vimeo.com/70591644 => 70591644
-    /// www.vimeo.com/70591644 => 70591644
-    /// vimeo.com/70591644 => 70591644
+    /// [
+    ///       'https://vimeo.com/1234323',
+    ///       'https://vimeo.com/channels/mychannel/1234323',
+    ///       'https://player.vimeo.com/video/1234323',
+    /// ]
     var vimeoVideoId = '';
-    var videoIdGroup = 4;
+    // var videoIdGroup = 4;
+    var videoIdGroup = 1;
     for (var exp in [
-      RegExp(r"^((https?)://)?(www.)?vimeo\.com/(\d+).*$"),
+      // RegExp(r"^((https?)://)?(www.)?vimeo\.com/(\d+).*$"),
+      RegExp(r'^(?:http|https)?:?/?/?(?:www\.)?(?:player\.)?vimeo\.com/(?:channels/(?:\w+/)?|groups/[^/]*/videos/|video/|)(\d+)(?:|/\?)?$'),
     ]) {
       RegExpMatch? match = exp.firstMatch(url);
       if (match != null && match.groupCount >= 1) {
         vimeoVideoId = match.group(videoIdGroup) ?? '';
       }
+    }
+
+    if(vimeoVideoId.isEmpty) {
+      throw(Exception('Unable extract video id from given vimeo video url: $url'));
     }
 
     final response = await _getVimeoVideoConfig(vimeoVideoId: vimeoVideoId);
